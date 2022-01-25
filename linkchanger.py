8cbc01b729cc6b2cd6fb81d545de1bc137ad7847
ANON-D46KPH4TOM <97580301+ANON-D46KPH4TOM@users.noreply.github.com>
2022-01-25 12:10:15 +0530

    Add files via upload

---
+++ linkchanger.py
@@ -0,0 +1,16 @@
+import pyshorteners
+print("""
+░██████╗██╗░░██╗░█████╗░██████╗░████████╗███████╗
+██╔════╝██║░░██║██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+╚█████╗░███████║██║░░██║██████╔╝░░░██║░░░█████╗░░
+░╚═══██╗██╔══██║██║░░██║██╔══██╗░░░██║░░░██╔══╝░░
+██████╔╝██║░░██║╚█████╔╝██║░░██║░░░██║░░░███████╗
+╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝                      """)
+print("""            created by Anon_d46kph4tom""")
+
+link=input(" Enter your link: ")
+print("*******************please wait******")
+cont=input("press enter to continue:")
+shortener=pyshorteners.Shortener()
+final=shortener.tinyurl.short(link)
+print(" Your url is  :"+final)
